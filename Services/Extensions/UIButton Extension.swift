dcd13e3baa016c8aaeb885622156ffc22cb81686
screamo819 <98119562+screamo819@users.noreply.github.com>
2022-05-28 22:49:52 +0300

    Create UIButton Extension.swift

---
+++ Services/Extensions/UIButton Extension.swift
@@ -0,0 +1,35 @@
+//
+//  UIButton Extension.swift
+//  Services
+//
+//  Created by AnnaEvgen on 28.05.2022.
+//
+
+import UIKit
+
+public extension UIButton {
+    
+    func startAnimatingPressActions() {
+        addTarget(self, action: #selector(animateDown), for: [.touchDown, .touchDragEnter])
+        addTarget(self, action: #selector(animateUp), for: [.touchDragExit, .touchCancel, .touchUpInside, .touchUpOutside])
+    }
+    
+    @objc private func animateDown(sender: UIButton) {
+        animate(sender, transform: CGAffineTransform.identity.scaledBy(x: 0.90, y: 0.90))
+    }
+    
+    @objc private func animateUp(sender: UIButton) {
+        animate(sender, transform: .identity)
+    }
+    
+    private func animate(_ button: UIButton, transform: CGAffineTransform) {
+        UIView.animate(withDuration: 0.4,
+                       delay: 0,
+                       usingSpringWithDamping: 0.5,
+                       initialSpringVelocity: 3,
+                       options: [.curveEaseInOut],
+                       animations: {
+            button.transform = transform
+        }, completion: nil)
+    }
+}
