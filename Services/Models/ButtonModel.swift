6c76cc607a9a10e4f051799ea16c70dcb51d0953
screamo819 <98119562+screamo819@users.noreply.github.com>
2022-05-28 22:48:36 +0300

    Create ButtonModel.swift
    that's all

---
+++ Services/Models/ButtonModel.swift
@@ -0,0 +1,13 @@
+//
+//  ButtonModel.swift
+//  Services
+//
+//  Created by AnnaEvgen on 28.05.2022.
+//
+
+import Foundation
+
+struct ButtonModel {
+    let title: String
+    let closure: () -> Void
+}
